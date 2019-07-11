79933311b4534dd46a9372a668ead4fb4582e9ef
adamquan <adam.quan@elastic.co>
2019-07-11 00:32:00 -0500

    Commented out existing instrumentation

--- src/main/java/org/springframework/samples/petclinic/validation/ZipCodeValidator.java
+++ src/main/java/org/springframework/samples/petclinic/validation/ZipCodeValidator.java
@@ -5,9 +5,9 @@ import javax.validation.ConstraintValidatorContext;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import co.elastic.apm.api.CaptureSpan;
-import co.elastic.apm.api.ElasticApm;
-import co.elastic.apm.api.Transaction;
+//import co.elastic.apm.api.CaptureSpan;
+//import co.elastic.apm.api.ElasticApm;
+//import co.elastic.apm.api.Transaction;
 
 public class ZipCodeValidator implements ConstraintValidator<ZipCodeConstraint,String> {
 
@@ -19,17 +19,19 @@ public class ZipCodeValidator implements ConstraintValidator<ZipCodeConstraint,S
         this.zipPattern = Pattern.compile("^([\\d]+){5}(?:-([\\d]+){4})?$");
     }
 
-    @CaptureSpan(value = "validateZipCode")
+    //@CaptureSpan(value = "validateZipCode")
     @Override
     public boolean isValid(String value, ConstraintValidatorContext context) {
         Matcher matcher = zipPattern.matcher(value);
         boolean match =  matcher.find();
+        /**
         if(!match) {
             Transaction transaction = ElasticApm.currentTransaction();
             if(transaction != null) {
                 transaction.captureException(new IllegalArgumentException(String.format("%s is invalid zip code", value)));
             }
         }
+         */
         return match;
     }
 }
